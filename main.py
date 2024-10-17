02b51ed417b05ec9bdad507a4898df76bace104d
carlos <carlosjchamorro@gmail.com>
2024-10-18 01:03:32 +0200

    FastAPI + Database Connections Libraries

---
+++ main.py
@@ -0,0 +1,81 @@
+from fastapi import FastAPI
+import jaydebeapi
+import pydantic
+import mariadb
+from sqlalchemy import create_engine, text
+
+
+# pip freeze > requirements.txt
+
+config = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "password": "12345",
+    "database": "Peliculas_JDBC",
+}
+
+# def execute_query(query):
+#     result = None 
+#     conn = None  
+#     cursor = None  
+#     try:
+#         conn = mariadb.connect(**config)
+#         cursor = conn.cursor()
+#         print("Connected to MariaDB")
+#         cursor.execute(query)
+#         result = cursor.fetchall()
+#     except mariadb.Error as e:
+#         print(f"Error connecting to MariaDB: {e}")
+#     finally:
+#         if cursor:
+#             cursor.close()
+#         if conn:
+#             conn.close()
+#         print("Connection closed")
+#     return result
+
+##############
+# jaydebeapi #
+##############
+    
+conn = jaydebeapi.connect(
+    "org.mariadb.jdbc.Driver",
+    "jdbc:mariadb://localhost:3306/Peliculas_JDBC",
+    ["root", "12345"],
+    "C:/DEV/PROYECTOS/PYTHON/JDBCFastAPI/lib/mariadb-java-client-3.4.1.jar",
+)
+
+curs = conn.cursor()
+curs.execute("SELECT * FROM Peliculas")
+result = curs.fetchall()
+print(result)
+print(type(result))
+
+
+##############
+# SQLalchemy #
+##############
+
+# engine = create_engine("mariadb+mariadbconnector://<user>:<password>@<host>[:<port>]/<dbname>")
+engine = create_engine("mariadb+mariadbconnector://root:12345@localhost:3306/Peliculas_JDBC")
+
+connection = engine.connect()
+print("sqlalchemyyy")
+result_sqlal = connection.execute(text("SELECT * FROM Peliculas")).fetchall()
+print(result_sqlal)
+print(type(result_sqlal))
+
+
+#######
+# API #    
+#######
+
+app = FastAPI()
+@app.get("/")
+async def read_root():
+    return str(result_sqlal)
+    # return result
+
+
+
