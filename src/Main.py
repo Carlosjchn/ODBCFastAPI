d2cf7de85acd9e60096dd7ad60c4daeab0df6e33
carlos <carlosjchamorro@gmail.com>
2024-10-25 08:51:21 +0200

    cambio documentacion

--- src/Main.py
+++ src/Main.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI, HTTPException
-from .routes.UsersRouters import router as router_usuario
+from .routes.UsersRouters import router as Prueba_router
 
 
 app = FastAPI(
-    title="Gestor Horarios",
-    description="Esta API almacena los datos de los horarios de cada usuarios que"+
-                "pertenecen a equipos de trabajo, guardando los horarios personales de cada usuario.",
-    version="1.0.0",
-    
+    title="API Horarios",
+    description="---\n"
+    "Esta **API** almacena los datos de los horarios de cada usuarios que"+
+    "pertenecen a equipos de trabajo, guardando los horarios personales de cada usuario.\n"+
+    "\n" +
+    "---",
+    version="0.1.0",
 )
 
 
 # Async function to call the synchronous query function
-app.include_router(router_usuario, 
-                   prefix="/Users",
-                   tags=["Users endpoints"],
-                   )
-
+app.include_router(Prueba_router, prefix="/Users", tags=["ENDPOINTS USUARIOS"])
 
