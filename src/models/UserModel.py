91d1215cdeb607fab714ee8e993ab104cd441922
Carlosjchn <carlosjchamorro@gmail.com>
2024-10-29 00:50:42 +0100

    correccion

--- src/models/UserModel.py
+++ src/models/UserModel.py
@@ -40,11 +40,6 @@ def User_Details_Response(user_results: List[tuple]) -> List[UserDetails]:
         # Descomponer la fila en un diccionario
         row_dict = dict(zip(
             ("id_usuario", "tipo", "nombre", "email", "password", "id_equipo",
-            "id_equipo_e",
-            "tipo",
-            "nombre",
-            "horas_inicio_act" ,
-            "hora_fin_act",
              "id_usuario_h","id_horario", "fecha", "hora_inicio", "hora_fin"), row))
         
         # Obtener o crear el objeto UserDetails
@@ -78,12 +73,5 @@ def extraer_horarios(row_dict, horarios_data):
     # Convertir los horarios a objetos Horario usando el método Horario_Default_Response
     horarios = Horario_Default_Response(horarios_data)
     return horarios
-def extraer_equipos(row_dict, horarios_data):
-    # Agregar el horario correspondiente a la lista
-    if row_dict["id_equipo_e"] is not None:
-            horarios_data.append((row_dict["id_usuario_h"],row_dict["id_horario"], row_dict["fecha"], row_dict["hora_inicio"], row_dict["hora_fin"]))
-            
-    # Convertir los horarios a objetos Horario usando el método Horario_Default_Response
-    horarios = Horario_Default_Response(horarios_data)
-    return horarios
+
 
